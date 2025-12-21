2e27f4bf1033da0993fc779ea447b3e7a0e226e2
Ritesh Gupta <kaviriteshgupta@gmail.com>
2025-12-21 17:06:23 +0530

    wildcard domain fix

--- backend/internal/dockerrun/dockerrun.go
+++ backend/internal/dockerrun/dockerrun.go
@@ -47,7 +47,7 @@ func (r *Runner) Run(ctx context.Context, imageName, subdomain, baseDomain strin
 		"traefik.http.routers." + routerName + ".rule":                       fmt.Sprintf("Host(`%s`)", fqdn),
 		"traefik.http.routers." + routerName + ".entrypoints":                "websecure",
 		"traefik.http.routers." + routerName + ".tls":                        "true",
-		"traefik.http.routers." + routerName + ".tls.certresolver":           "le",
+		"traefik.http.routers." + routerName + ".tls.certresolver":           "letsencrypt",
 		"traefik.http.services." + serviceName + ".loadbalancer.server.port": strconv.Itoa(internalPort),
 	}
 
